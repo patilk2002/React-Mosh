91d38da136c981922467d52ec2807aa330088335
Kiran <kiran.kp2004@gmail.com>
2023-08-23 12:03:53 +0530

    List and functions

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,12 +1,54 @@
+// import { Fragment } from "react";
+import { MouseEvent } from "react";
+
 function ListGroup() {
+  let items = ["India", "USA", "Rassia", "China", "Singapore", "japan"];
+  //   items = [];
+  let selectedindx = 0;
+
+  //eventhandler
+  const handleclick = (event: MouseEvent) => console.log(event);
+
+  //   if (items.length == 0)
+  //     return (
+  //       <>
+  //         <h1>List</h1>
+  //         <p>Item not found</p>
+  //       </>
+  //     );
+
+  function getmsg() {
+    const msg = items.length === 0 ? <p>Item not found</p> : null;
+  }
+
   return (
-    <ul className="list-group">
-      <li className="list-group-item">An item</li>
-      <li className="list-group-item">A second item</li>
-      <li className="list-group-item">A third item</li>
-      <li className="list-group-item">A fourth item</li>
-      <li className="list-group-item">And a fifth one</li>
-    </ul>
+    // <Fragment>
+    <>
+      <h1>List</h1>
+      {/* {msg} */}
+      {getmsg()}
+
+      {/* {items.length === 0 ? <p>Item not found</p> : null} */}
+      {/* {items.length === 0 && <p>Item not found</p>} */}
+
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedindx === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            // onClick={() => console.log(item, index)}
+            onClick={handleclick}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+    // </Fragment>
   );
 }
 
